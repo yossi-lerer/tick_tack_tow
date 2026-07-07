6cb72c90a28f6536e4a8e4852f4a1d79dd7b6d80
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-07 14:01:28 +0300

    change the name of varible and add guide for input move

--- game.py
+++ game.py
@@ -4,18 +4,18 @@ from board import create_board, print_board, place_symbol
 def play_single_player_game():
     player_name = ask_player_name()
     symble = "X"
-    c_boars = create_board()
-    show_board = print_board(c_boars)
+    create_statrt_board = create_board()
+    show_board = print_board(create_statrt_board)
     round = 0
     while round < 9:
         turn_message = show_turn_message(player_name, symble)
         move = ask_move()
-        place = place_symbol(c_boars, move[0], move[1], symble)
-        show_after_place = print_board(c_boars)
+        place = place_symbol(create_statrt_board, move[0], move[1], symble)
+        show_after_place = print_board(create_statrt_board)
         round += 1
 
 def ask_move() -> tuple[int, int]:
-    move = input("please enter two number for your next move first num for row and secend num for column. for exemple 1.2 ")
+    move = input("please enter two number for your next move first num for row and secend num for column. Mandatory separation of numbers with a dot. for exemple 1.2 ")
     move = move.split(".")
     move[0] = int(move[0])
     move[1] = int(move[1])

e084a73b2c23a8c712e83bf14919c5b696189a71
Elyashiv <elyashiv988@gmail.com>
2026-07-07 16:47:29 +0300

    fixed menu

--- menu.py
+++ menu.py
@@ -3,5 +3,10 @@ def show_nain_menu():
     user_choose=input("  \n### tic tack tow ###\n\nDeveloped by Elyashiv Cohen, Chaim Doitch, Yossi Lerer, Aarye Naori\n\n* Menu *\n1). Play\n2). Exit\nEnter your choose: ")
     if user_choose=="2":
         exit
-    else:
+    elif user_choose=="1":
         return True
+    else:
+        print("invlaid menu, try agein!")
+        user_choose=input("  \n### tic tack tow ###\n\nDeveloped by Elyashiv Cohen, Chaim Doitch, Yossi Lerer, Aarye Naori\n\n* Menu *\n1). Play\n2). Exit\nEnter your choose: ")
+
+

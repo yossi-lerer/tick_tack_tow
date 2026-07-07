56d5bca9d09673959aa2041b3f2eaf6e932a490a
arienaouri <lion05003@gmail.com>
2026-07-07 11:32:34 +0300

    fixed final2.0

--- board.py
+++ board.py
@@ -1,15 +1,16 @@
 def create_board():
-    row=3 #שורה
-    col=3 # עמודה
+    row=3 
+    col=3 
     list_of_borad=[["*" for _ in range(col)] for _ in range(row)]
     return list_of_borad
 def print_board(board):
      row=len(board)
      col=row
-     for i in range(row):
+     for row1 in range(row):
+        for col1 in range(col):
+            print(board[row1][col1] ,end= "")
         print("")
-        for j in range(col):
-            print(board[i][j] ,end= "")
+        
     
 def place_symbol(board,row,col,symbol):
     board[row][col] = symbol

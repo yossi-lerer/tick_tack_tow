fdb8e6e1cca51801055adaed21da6492c682ecda
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-07 11:51:32 +0300

    first v to check the funcnion

--- game.py
+++ game.py
@@ -1,5 +1,18 @@
+from players import ask_player_name, show_turn_message
+from board import create_board, print_board, place_symbol
+
 def play_single_player_game():
-    pass
+    player_name = ask_player_name()
+    symble = "X"
+    round = 0
+    while round < 9:
+        c_boars = create_board()
+        turn_message = show_turn_message(player_name, symble)
+        show_board = print_board(c_boars)
+        move = ask_move()
+        place = place_symbol(c_boars, move[0], move[1], symble)
+        show_after_place = print_board(c_boars)
+        round += 1
 
 def ask_move() -> tuple[int, int]:
     move = input("please enter two number for your next move first num for row and secend num for column. for exemple 1.2 ")
@@ -7,4 +20,4 @@ def ask_move() -> tuple[int, int]:
     move[0] = int(move[0])
     move[1] = int(move[1])
     return move[0], move[1]
-print(ask_move())
+play_single_player_game()

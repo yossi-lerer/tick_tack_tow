0695078c52c2dc67f5733c9408a9d830db1d26fa
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-07-07 10:55:21 +0300

    create the progress to get two numbers and convert to numbers and return a tuple with two numbers

--- game.py
+++ game.py
@@ -1,4 +1,10 @@
 def play_single_player_game():
     pass
+
 def ask_move() -> tuple[int, int]:
-    pass
+    move = input("please enter two number for your next move first num for row and secend num for column. for exemple 1.2 ")
+    move = move.split(".")
+    move[0] = int(move[0])
+    move[1] = int(move[1])
+    return move[0], move[1]
+print(ask_move())
